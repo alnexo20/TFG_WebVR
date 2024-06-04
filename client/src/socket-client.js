b4438b99d8b2f9ee81b62848498e9538086bd7bc
alexurrutiablockets <70670999+alnexo20@users.noreply.github.com>
2024-06-04 20:36:20 +0200

    connect socketio

--- client/src/socket-client.js
+++ client/src/socket-client.js
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://tfg-web-vr-server.vercel.app');
+const socket = io('https://tfg-web-vr-server.vercel.app', {
+  transports: ['websocket'],
+  secure: true,
+});
 
 export default socket;
