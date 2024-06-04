cbd80e585368260b677c7a0a0bb3dc8a3e2823e0
alexurrutiablockets <70670999+alnexo20@users.noreply.github.com>
2024-06-04 19:25:41 +0200

    update server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 // Serve static files from the React app
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Serve the React app for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 let currentColor = '#FFFFFF'; // Initial color
 
