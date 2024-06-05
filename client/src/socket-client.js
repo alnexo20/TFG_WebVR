3c2fc3ef2c04ee7c66fe2be3505468c01163442d
alexurrutiablockets <70670999+alnexo20@users.noreply.github.com>
2024-06-05 02:08:47 +0200

    test glitch

--- client/src/socket-client.js
+++ client/src/socket-client.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://tfg-web-vr-server.vercel.app', {
+const socket = io('https://brief-husky-nymphea.glitch.me', {
+  extraHeaders: {
+    "user-agent": "Mozilla",
+  },
+  path: '/socket.io',
   transports: ['websocket'],
   secure: true,
 });
